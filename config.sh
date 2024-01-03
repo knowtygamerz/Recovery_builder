797f2fe689a23dc24a326f9857aa1dc14aa7d621
knowtygamerz <112211646+knowtygamerz@users.noreply.github.com>
2024-01-03 16:49:34 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,8 +7,8 @@ export BRANCH="twrp-12.1; # the branch of manifest
 
 # about your device
 export DEVICE="RMX3782" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
+export DT_LINK="https://github.com/knowtygamerz/twrp_devive_realme_RMX3782.git" # device tree link
+export DT_BRANCH="main" # device tree branch
 export VENDOR="realme" # device manufacturer or vendor
 export TARGET="vendorbootimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="aosp" # recovery type: omni, twrp, something else
